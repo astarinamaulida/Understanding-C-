28f00ad89ab21e5ae0075a9eafbccc6f2cbd3341
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-16 02:16:15 -0500

    Run my first C# codes

---
+++ Program.cs
@@ -0,0 +1,20 @@
+////////// Run Some C# //////////
+
+// Console.WriteLine() is a command that prints text to a console
+// Like console.log() in js
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello Asta!");    
+     }
+  }
+}
+
+
+ 
